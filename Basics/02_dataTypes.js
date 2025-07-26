4070d326fae521397f6ac0e022a5fd288312e038
SaadAltaf19 <135546558+SaadAltaf19@users.noreply.github.com>
2025-07-26 12:32:08 +0000

    DataTypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //the text contain your js file as a newer version of js
+
+//alert(1+45) // we are using node.js not browser, this method is for browser
+
+let name = "Saad"
+let age = 21
+let isLoggedIn = false
+
+console.log(name,age,isLoggedIn);
+console.table([name,age,isLoggedIn])
+
+//number => 2 to power 53
+//bigint => this is use in trading/Stock Market etc, there was use in large numbers
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique: when you use more component so you can call that this component is unique so there was this is use , and more used in REACT
+
+//object
+
+console.log(typeof undefined);
