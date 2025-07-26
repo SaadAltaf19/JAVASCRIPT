c17ab5edcbebd5b8e07b8bd1a32b667ad2414da5
SaadAltaf19 <135546558+SaadAltaf19@users.noreply.github.com>
2025-07-26 11:21:06 +0000

    Const and Let, Variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountEmail = "saadmamdani18@gmail.com"
+var accountPassword = "678910"
+accountCity = "Karachi"
+
+//accountId = 2 //changing in const variable is not allowed
+
+accountEmail = "mamdani628@gmail.com"
+accountPassword = "123456"
+accountCity = "Lahore"
+let accountState = "Sindh";
+
+console.log(accountId,accountEmail,accountPassword,accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
