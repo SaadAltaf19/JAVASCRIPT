d30ab97e5198cdd10e4ad687b865150742eb8e76
SaadAltaf19 <135546558+SaadAltaf19@users.noreply.github.com>
2025-07-30 18:35:22 +0000

    Conversion And Operations

--- Basics/03_conversionOperations.js
+++ Basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "Saad"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //NaN means not a number
 
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = "saad"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=> false
 // "" => false
@@ -24,6 +24,35 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber)
+
+// ****************************** Operations *****************************
+
+let value = 3
+let negativeNumber = -value
+//console.log(negativeNumber);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**3);
+//console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Saad"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log(+true);
+//console.log(+"");
+
+// The "prefix" form returns the new value while the "postfix" form returns the old value
 
